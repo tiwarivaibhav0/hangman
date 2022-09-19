414ce8de4564991acbc115ecd4873b8e2cd9430f
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-09-19 13:31:53 +0530

    two config

--- config.dev.js
+++ config.dev.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/app.js"),
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
@@ -10,5 +12,13 @@ module.exports = {
     hot: true,
     compress: true,
     historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
